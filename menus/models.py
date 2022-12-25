1188f05763342b00d416691713e3ec0eb98f4f37
MinnullinIlshat <minnullinilshat@gmail.com>
2022-12-25 07:32:23 +0300

    add main_menu and common tags

--- menus/models.py
+++ menus/models.py
@@ -3,17 +3,16 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField("Название", max_length=100)
-    url = models.CharField("Ссылка", max_length=255)
-    parent = models.CharField("Имя родителя", max_length=100, default='')
+    url = models.CharField("Ссылка", max_length=255, null=True, blank=True)
 
+    top_position = models.PositiveIntegerField("Позиция главного пункта", default=0)
     position = models.PositiveIntegerField("Позиция", default=1)
-    level = models.PositiveIntegerField("Уровень вложенности", default=0)
-    top_level = models.PositiveIntegerField("Позиция корня", default=1)
+    lvl = models.PositiveIntegerField("Уровень", default=0)
 
     def __str__(self):
         return self.name 
 
     class Meta: 
-        ordering = ('top_level', 'level', 'position')
+        ordering = ('top_position', 'lvl', 'position')
         verbose_name = 'Пункт меню'
         verbose_name_plural = 'Пункты меню'
--- menus/templatetags/common_tags.py
+++ menus/templatetags/common_tags.py
@@ -3,8 +3,15 @@ from menus.models import Menu
 register = template.Library() 
 
 
-@register.inclusion_tag('menu.html', takes_context=True)
-def draw_menu(context):
+@register.inclusion_tag('menu.html')
+def draw_menu(menu_name, tp=None, lvl=None, pos=None):
+    menu_collection = {
+        'main_menu': main_menu
+    }
+    return menu_collection[menu_name](tp, lvl, pos)
+
+
+def main_menu(tp, lvl, pos):
     menu_items = Menu.objects.all() 
     return {
         "menu_items": menu_items,
